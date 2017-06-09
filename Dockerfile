a08190752cdf00d008ed81877a1b2852c0b24e1e
Ezbon Jacob <ezjacob@gmail.com>
2017-06-09 16:17:03 -0400

    updated default startup.sh script [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN apt-get -y install netcat
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
-# RUN npm install -g grunt-cli bower
+ADD startup.sh .
 
 EXPOSE 9000
 CMD [ "./startup.sh" ]
--- startup.sh
+++ startup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Install app dependencies
-npm install
-
-# wait for database container to startup
-while ! nc -w 1 -z db 2424; do echo "waiting for port"; sleep 1; done
-
-npm start
+bash
